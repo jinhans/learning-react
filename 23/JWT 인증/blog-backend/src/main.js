dc2939d9d2610ce4474e5c750ba8fcf8bbfcb869
jinhans <wlsgks846@gmail.com>
2023-02-06 15:27:32 +0900

    토큰 검증하기

--- 23/JWT 인증/blog-backend/src/main.js
+++ 23/JWT 인증/blog-backend/src/main.js
@@ -5,17 +5,18 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
-.connect(MONGO_URI)
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch(e => {
-  console.error(e);
-});
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const app = new Koa();
 const router = new Router();
@@ -23,6 +24,7 @@ const router = new Router();
 router.use('/api', api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
 
