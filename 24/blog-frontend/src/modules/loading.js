5a5cc7ba8759a038bc6134f9faf4e04a63652975
jinhans <wlsgks846@gmail.com>
2023-02-15 15:19:45 +0900

    auth 리덕스 모듈에서 API 사용하기

--- 24/blog-frontend/src/modules/loading.js
+++ 24/blog-frontend/src/modules/loading.js
@@ -4,6 +4,11 @@ const START_LOADING = 'loading/START_LOADING';
 const FINISH_LOADING = 'loading/FINISH_LOADING';
 
 export const startLoading = createAction(
+  START_LOADING,
+  (requestType) => requestType,
+);
+
+export const finishLoading = createAction(
   FINISH_LOADING,
   (requestType) => requestType,
 );
