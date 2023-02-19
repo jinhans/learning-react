4e2c12ed097995692fd0688c7782b5a8cb439211
jinhans <wlsgks846@gmail.com>
2023-02-19 22:08:23 +0900

    헤더 컴포넌트 만들기

--- 24/blog-frontend/src/components/common/Button.js
+++ 24/blog-frontend/src/components/common/Button.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
-  font-weight: blod;
+  font-weight: bold;
   padding: 0.25rem 1rem;
   color: white;
   outline: none;
@@ -35,6 +37,20 @@ const StyledButton = styled.button`
     `}
 `;
 
-const Button = (props) => <StyledButton {...props} />;
+const StyledButton = styled.button`
+  ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+  ${buttonStyle}
+`;
+
+const Button = (props) => {
+  return props.to ? (
+    <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
+  ) : (
+    <StyledButton {...props} />
+  );
+};
 
 export default Button;
